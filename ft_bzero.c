54ce5b62b4d46bad8f8bf37a4657a95220271ac7
Cellygomesz <tetellygomeszinha@gmail.com>
2024-09-02 21:40:16 -0300

    fiz a bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/01 21:24:57 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/09/02 21:38:32 by mgomes-s         ###   ########.fr       */
+/*   Created: 2024/09/02 21:17:16 by mgomes-s          #+#    #+#             */
+/*   Updated: 2024/09/02 21:32:55 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*ptr;
 
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	ptr = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}

bb0896d3d817aed0d405ae9c74adac451749b796
mgomes-s <marcellygomes003@gmail.com>
2024-09-25 16:02:31 -0300

    que dor de cabeça

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -24,22 +24,9 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 
     while (i < n && (s01[i] || s02[i]))
     {
-        if (s01 > s02)
-            return (1);
-        else if (s01 < s02)
-            return (-1);
-        else
-            return (0);
+        if (s01[i] != s02[i])
+            return (*s01 - *s02);
+        i++;
     }
     return(0);
-}
-
-#include <stdio.h>
-#include <string.h>
-int main(void)
-{
-    // quando s1 é maior, o valor é positivo. quando s2 é maior, o valor é negativo. quando é igual, o valor é 0.
-    printf("original: %d\n", memcmp("lov9", "lov", 4));
-    printf("minha: %d\n", ft_memcmp("lov9", "lov", 4));
-    return (0);
 }

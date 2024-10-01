9dddf1f833852acd11981584205d342a9a015648
mgomes-s <marcellygomes003@gmail.com>
2024-10-01 12:21:26 -0300

    preciso consertar

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,15 +6,20 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:46:41 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/09/28 18:51:18 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/01 10:23:47 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	*ptr;
-	ptr = (size_t *)malloc(nmemb * size);
-	ft_bzero(nmemb, size);
+	size_t	allsize;
+	void	*ptr;
+
+	allsize = nmemb * size;
+	ptr = (void *)malloc(allsize);
+	if (ptr != NULL)
+		ft_memset(ptr, 0, allsize);
+	return (ptr);
 }

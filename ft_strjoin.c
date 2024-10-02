c67c6a905a82fe13c98ea0d8d470961c9f2b0a96
mgomes-s <marcellygomes003@gmail.com>
2024-10-02 08:55:37 -0300

    função

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 10:36:52 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/01 15:00:38 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/02 08:51:36 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strjoin( char const *s1, char const *s2)
 	j = 0;
 	if (s01 == NULL || s02 == NULL)
 		return (NULL);
-	ptr = (char *)malloc((ft_strlen(s01) + ft_strlen(s02))  + 1);
+	ptr = (char *)malloc((ft_strlen(s01) + ft_strlen(s02)) + 1);
 	while (s01[i])
 	{
 		ptr[i] = s01[i];
@@ -34,11 +34,9 @@ char	*ft_strjoin( char const *s1, char const *s2)
 	}
 	while (s02[j])
 	{
-		ptr[i + j]  = s02[j];
+		ptr[i + j] = s02[j];
 		j++;
 	}
 	ptr[i + j] = '\0';
 	return (ptr);
 }
-
-

d2aeecf1be61fdd289f4cc128ac2d9a698d02809
mgomes-s <marcellygomes003@gmail.com>
2024-10-02 15:26:34 -0300

    função

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,50 +6,24 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:01:58 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/02 12:12:41 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/02 15:22:03 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// Refazer !!!!!!! 
 
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s01;
-	char	*ptr;
 	int		i;
 	int		j;
-	int		k;
 
 	i = 0;
-	j = 0;
-	k = 0;
-	s01 = (char *)s1;
-	if (!s01)
-		return (NULL);
-	while (s01[i] != '\0')
-	{
-		if (s01[i] == set[j])
-		{
-			i++;
-			j++;
-		}
-		else if (s01[i] != set[j])
-		{
-			ptr[k] = s01[i];
-			i++;
-			j++;
-			k++;
-		}
-	}
-	return (ptr);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-	printf("%s\n", ft_strtrim("eu como batata", "como"));
-	return (0);
+	j = ft_strlen(s1) - 1;
+	while (i <= j && ft_strchr(set, s1[i]))
+		i++;
+	while (j >= i && ft_strchr(set, s1[j]))
+		j--;
+	if (((j - i) + 1) == 0)
+		return (ft_strdup(""));
+	return (ft_substr(s1, i, (j - i) + 1));
 }

3cd3e63e094be1182a69a0287e8d42726b8527f9
mgomes-s <marcellygomes003@gmail.com>
2024-10-03 15:50:04 -0300

    pituts eh b

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:07:58 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/09/26 19:12:05 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/03 15:45:56 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int		szdst;
-	int		i;
-	char	*ptrsrc;
+	size_t	lendst;
+	size_t	lensrc;
+	size_t	i;
+	size_t	allsize;
 
-	szdst = ft_strlen(dst);
 	i = 0;
-	ptrsrc = (char *)src;
-	while (src[i])
+	lendst = ft_strlen(dst);
+	lensrc = ft_strlen(src);
+	allsize = lendst + lensrc;
+	if (size <= lendst)
+		return (size + lensrc);
+	while (src[i] && (lendst + i) < (size - 1))
 	{
-		dst[szdst] = src[i];
-		szdst++;
+		dst[lendst + i] = src[i];
 		i++;
 	}
-	dst[szdst] = '\0';
-	return (0);
+	dst[lendst + i] = '\0';
+	return (allsize);
 }

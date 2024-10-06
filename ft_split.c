663b82568d21461cb81e47fb4a8de59f6c9ed409
Cellygomesz <tetellygomeszinha@gmail.com>
2024-10-05 21:56:34 -0300

    função

--- ft_split.c
+++ ft_split.c
@@ -5,54 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgomes-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/03 16:25:11 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/04 16:17:50 by mgomes-s         ###   ########.fr       */
+/*   Created: 2024/10/04 16:21:20 by mgomes-s          #+#    #+#             */
+/*   Updated: 2024/10/05 21:53:41 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-static int	count_words(const char *s, char c)
-{
-	int	i;
-	int	words;
-	int flag;
-
-	i = 0;
-	words = 0;
-	flag = 0;
-	while (s[i])
-	{
-		if ((s[i] == c || s[i + 1] == '\0') && (flag == 0))
-		{
-			words++;
-			flag = 1;
-		}
-		else if (s[i] != c)
-			flag = 0;
-		i++;
-	}
-	return (words);
-}
-
-static char	**create_words(char *s, int start, int end)
-{
-
-}
+static int	count_words(const char *s, char c);
+static void	create_result(char **result, const char *s, char c);
+static char	*create_word(const char *s, int start, int end);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
 
+	if (!s)
+		return (NULL);
 	result = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
-	create_words(result, s, c);
+	if (!result)
+		return (NULL);
+	create_result(result, s, c);
+	return (result);
 }
 
-#include <stdio.h>
-
-int	main(void)
+static int	count_words(const char *s, char c)
 {
-	printf("%i\n", ft_split("momo 42 tots   $   é muito tots", ' '));
-	return (0);
+	int	i;
+	int	words;
+	int	flag;
+
+	i = 0;
+	words = 0;
+	flag = 1;
+	while (s[i])
+	{
+		if (s[i] == c && flag == 0)
+			flag = 1;
+		else if (s[i] != c && flag == 1)
+		{
+			words++;
+			flag = 0;
+		}
+		i++;
+	}
+	return (words);
+}
+
+static void	create_result(char **result, const char *s, char c)
+{
+	int	i;
+	int	start;
+	int	word_index;
+
+	i = 0;
+	word_index = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			result[word_index] = create_word(s, start, i - 1);
+			word_index++;
+		}
+		else
+			i++;
+	}
+	result[word_index] = NULL;
+}
+
+static char	*create_word(const char *s, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	word = (char *)malloc((end - start + 2) * sizeof(char));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+	{
+		word[i++] = s[start++];
+	}
+	word[i] = '\0';
+	return (word);
 }

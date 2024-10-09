e3a76cbcf72d6b31623a1ec2fdcec47723cae7e7
mgomes-s <marcellygomes003@gmail.com>
2024-10-09 17:24:18 -0300

    bonus função

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@42.rio>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:40:12 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/09 14:56:14 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:35:12 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,34 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	i;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return (lst); 
+}
 
-	return ();
+#include <stdio.h>
+
+int main(void)
+{
+	t_list	node1;
+	t_list	node2;
+	t_list	node3;
+
+	char	*frase1 = "hello, word!";
+	char	*frase2 = "olá, mundo!";
+	char	*frase3 = "Bonjour le monde!"; // quero que me retorne essa aqui
+
+	node1.content = frase1;
+	node1.next = &node2;
+	node2.content = frase2;
+	node2.next = &node3;
+	node3.content = frase3;
+	node3.next = NULL;
+
+	t_list	*result = ft_lstlast(&node1);
+
+	printf("resultado: %s\n", (char *)result->content);
+	return (0);
 }
